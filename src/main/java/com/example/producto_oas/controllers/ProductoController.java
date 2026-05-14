0c9c6c221c2fcbba201bfd7c33b7b1b0020aae2a
Satansupremo <rastanhy@gmail.com>
2026-05-14 13:19:49 -0500

    Compactar 1

--- src/main/java/com/example/producto_oas/controllers/ProductoController.java
+++ src/main/java/com/example/producto_oas/controllers/ProductoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 // MANTENER
+// ULTIMO
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/producto")
